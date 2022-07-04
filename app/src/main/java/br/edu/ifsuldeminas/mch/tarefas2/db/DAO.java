13bee13bcf0d787a701edd78cde537ee3d6dba1a
LuizFelipeGatto <luizfelipegatto08@gmail.com>
2022-07-04 20:41:46 -0300

    first commit

---
+++ app/src/main/java/br/edu/ifsuldeminas/mch/tarefas2/db/DAO.java
@@ -0,0 +1,31 @@
+package br.edu.ifsuldeminas.mch.tarefas2.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.List;
+
+abstract class DAO<T> {
+    private DBHandler dbHandler = null;
+
+    DAO(Context context){
+        if (dbHandler == null){
+            dbHandler = new DBHandler(context);
+        }
+    }
+
+    SQLiteDatabase openToWrite(){
+        SQLiteDatabase sqLiteDatabase = dbHandler.getWritableDatabase();
+        sqLiteDatabase.setForeignKeyConstraintsEnabled(true);
+        return sqLiteDatabase;
+    }
+
+    SQLiteDatabase openToRead(){
+        return dbHandler.getReadableDatabase();
+    }
+
+    public abstract void save(T entity);
+    public abstract void update(T entity);
+    public abstract void delete(T entity);
+    public abstract List<T> listAll();
+}
